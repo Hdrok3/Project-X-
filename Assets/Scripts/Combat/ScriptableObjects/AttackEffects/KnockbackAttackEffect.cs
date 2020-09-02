710b6a9e07f46c1b302e515455836ac63cc7176a
Hdrok <mesutengin.3@gmail.com>
2020-09-02 22:30:51 +0300

    Test

---
+++ Assets/Scripts/Combat/ScriptableObjects/AttackEffects/KnockbackAttackEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Knockback Attack Effect", menuName = "Combat/AttackEffects/KnockbackEffect")]
+public class KnockbackAttackEffect : AttackEffect
+{
+    public float forceAmount;
+    public override void OnAttack(GameObject attacker, GameObject defender, AttackDefinition attackDefinition, Attack attack)
+    {
+        Rigidbody rb = defender.GetComponent<Rigidbody>();
+        Vector3 forceDir = (defender.transform.position - attacker.transform.position).normalized;
+        forceDir.y += 0.5f;
+        rb?.AddForce(forceDir * forceAmount);
+    }
+}
---
+++ Assets/Scripts/ScrollingText.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScrollingText : MonoBehaviour
+{
+    public float duration = 1f;
+    public float speed;
+
+    TextMeshPro textMesh;
+    Color textColor;
+    float startTime;
+    void Awake()
+    {
+        textMesh = GetComponent<TextMeshPro>();
+        startTime = Time.time;
+        transform.rotation = Camera.main.transform.rotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - startTime < duration)
+        {
+            // kameraya bakmasi gerekebilir
+            // transform.Translate(Vector3.up * speed * Time.deltaTime);
+            float percent = (duration - (Time.time - startTime));
+            textMesh.fontSize = Mathf.Lerp(1, 6, ((Time.time - startTime)*speed));
+            textColor.a =  percent + 0.5f;
+            textMesh.color = textColor;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetText(string text)
+    {
+        textMesh.text = text;
+    }
+
+    public void SetColor(Color color)
+    {
+        textColor = color;
+        textMesh.color = color;
+    }
+}
