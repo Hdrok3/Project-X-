ab0f1a300724e98ee83c242975d58361719cf474
alikayaalpp <alikayaalp38@gmail.com>
2020-09-12 01:35:50 +0300

    Main Menu
    Main Menu implementation

---
+++ Assets/Scripts/State/MainMenuState/MainMenuState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class MainMenuState :  MonoBehaviour
+{
+    protected virtual void Start()
+    {
+    }
+
+}
+
---
+++ Assets/Scripts/State/MenuState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Bu class MainMenuState'i kontrol eder.(Controller)
+public class MenuState : GameState
+{
+    private Dictionary<string, GameObject> _resourceObjects = new Dictionary<string, GameObject>();
+    private GameObject _currentState;
+
+    [SerializeField] private Button _shopButton;
+    [SerializeField] private Button _invabtoryButton;
+    [SerializeField] private Button _arcButton;
+    [SerializeField] private Button _skillButton;
+
+    private void Start()
+    {
+        Init();
+        _currentState = _resourceObjects["ArcState"];
+    }
+
+    private void Init()
+    {
+        _resourceObjects.Add("ShopState", Resources.Load<GameObject>("State/ShopState"));
+        _resourceObjects.Add("InvantoryState", Resources.Load<GameObject>("State/InvantoryState"));
+        _resourceObjects.Add("ArcState", Resources.Load<GameObject>("State/ArcState"));
+        _resourceObjects.Add("SkillState", Resources.Load<GameObject>("State/SkillState"));
+    }
+
+    private void ChangeState(string _newStateName)
+    {
+        _currentState.gameObject.SetActive(false);
+        _currentState = _resourceObjects[_newStateName];
+        _currentState.gameObject.SetActive(true);
+    }
+}
