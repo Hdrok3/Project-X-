38d6ce3dcfc5e3efe3056802671505b0e2f15cac
alikayaalpp <alikayaalp38@gmail.com>
2020-09-15 00:33:05 +0300

    Maim Menu State is done but no Factory implementation yet.

--- Assets/Scripts/Factory/Factory.cs
+++ Assets/Scripts/Factory/Factory.cs
@@ -4,18 +4,4 @@ using UnityEngine;
 
 public abstract class Factory
 {
-    protected static Dictionary<System.Type, Factory> _allFactories = new Dictionary<System.Type, Factory>();
-
-    public static Dictionary<System.Type, Factory> AllFactories
-    {
-        get { return _allFactories; }
-    }
-
-    public Factory()
-    {
-        if (!_allFactories.ContainsKey(GetType()))
-        {
-            _allFactories.Add(GetType(), this);
-        }
-    }
 }
--- Assets/Scripts/State/MenuState.cs
+++ Assets/Scripts/State/MenuState.cs
@@ -6,32 +6,65 @@ using UnityEngine.UI;
 // Bu class MainMenuState'i kontrol eder.(Controller)
 public class MenuState : GameState
 {
-    private Dictionary<string, GameObject> _resourceObjects = new Dictionary<string, GameObject>();
-    private GameObject _currentState;
+    private Dictionary<string, MainMenuState> _resourceObjects = new Dictionary<string, MainMenuState>();
+    private MainMenuState _currentState;
+
 
     [SerializeField] private Button _shopButton;
-    [SerializeField] private Button _invabtoryButton;
+    [SerializeField] private Button _invantoryButton;
     [SerializeField] private Button _arcButton;
     [SerializeField] private Button _skillButton;
 
     private void Start()
     {
         Init();
-        _currentState = _resourceObjects["ArcState"];
+        SetButtonEvents();
+        _currentState = _resourceObjects["ArcState"].GetComponent<MainMenuState>();
     }
 
     private void Init()
     {
-        _resourceObjects.Add("ShopState", Resources.Load<GameObject>("State/ShopState"));
-        _resourceObjects.Add("InvantoryState", Resources.Load<GameObject>("State/InvantoryState"));
-        _resourceObjects.Add("ArcState", Resources.Load<GameObject>("State/ArcState"));
-        _resourceObjects.Add("SkillState", Resources.Load<GameObject>("State/SkillState"));
+        _resourceObjects.Add("ShopState", Resources.Load<GameObject>("State/ShopState").GetComponent<MainMenuState>());
+        _resourceObjects.Add("InvantoryState", Resources.Load<GameObject>("State/InvantoryState").GetComponent<MainMenuState>());
+        _resourceObjects.Add("ArcState", Resources.Load<GameObject>("State/ArcState").GetComponent<MainMenuState>());
+        _resourceObjects.Add("SkillState", Resources.Load<GameObject>("State/SkillState").GetComponent<MainMenuState>());
+
+        ChangeState(_resourceObjects["ArcState"]);
     }
 
-    private void ChangeState(string _newStateName)
+    private void ChangeState(MainMenuState newState)
     {
-        _currentState.gameObject.SetActive(false);
-        _currentState = _resourceObjects[_newStateName];
-        _currentState.gameObject.SetActive(true);
+        _currentState?.OnStateExit();
+        _currentState = newState;
+        _currentState.OnStateEnter(this.gameObject.transform);
     }
+
+    private void SetButtonEvents()
+    {
+        _arcButton.onClick.AddListener(OnClickArcButton);
+        _invantoryButton.onClick.AddListener(OnClickInvantoryButton);
+        _shopButton.onClick.AddListener(OnClickShopButton);
+        _skillButton.onClick.AddListener(OnClickSkillButton);
+    }
+
+    private void OnClickShopButton()
+    {
+        ChangeState(_resourceObjects["ShopState"]);
+    }
+
+    private void OnClickInvantoryButton()
+    {
+        ChangeState(_resourceObjects["InvantoryState"]);
+    }
+
+    private void OnClickArcButton()
+    {
+        ChangeState(_resourceObjects["ArcState"]);
+    }
+
+    private void OnClickSkillButton()
+    {
+        ChangeState(_resourceObjects["SkillState"]);
+    }
+
 }
